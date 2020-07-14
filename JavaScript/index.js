9d211a5feef1364aec09864bd5748a9f0e0bbf92
Sagui10 <marc10sagues@gmail.com>
2020-07-14 19:42:44 +0200

    2

---
+++ JavaScript/index.js
@@ -0,0 +1,11 @@
+alert("Hola")
+var position = $(window).scrollTop(); 
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if(scroll > position) {
+            alert('scroll abajo');
+    } else {
+        alert('scroll arriba');
+    }
+    position = scroll;
+});
