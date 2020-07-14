85a50efa1fbfad16673677ebaafd1706fe3d7f03
Oriol <oriol119@gmail.com>
2020-07-14 20:42:26 +0200

    .

--- JavaScript/Afiliado_hover.js
+++ JavaScript/Afiliado_hover.js
@@ -2,14 +2,21 @@
  
 var theLink = document.querySelector("a[name='opcion1']");
 
-
 // "Wire" the element's events
 theLink.addEventListener("mouseover", mouseOver);
 theLink.addEventListener("mouseout", mouseOut);
 
 
-function mouseOver() {
+
+function mouseOut() {
  
+	hideImage();
+
+}
+
+function mouseOver() {
+ 	
+
 	showImage();
 
 }
@@ -23,6 +30,12 @@ function showImage(){
 	}
 }
 
+function hideImage(){
+	var imgs = document.getElementsByTagName("img");
+	for (var i = 0; i < imgs.length; i++) {
+		imgs[i].style.visibility = "hidden";
+	}
+}
 
 
 
