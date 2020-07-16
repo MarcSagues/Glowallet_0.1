52fc03949f7adc3166a00d39db29e143bf98e9ab
Oriol <oriol119@gmail.com>
2020-07-16 15:15:36 +0200

    .

---
+++ JavaScript/Afiliado_video.js
@@ -0,0 +1,19 @@
+
+
+var video = document.getElementById("video1");
+var boton = document.getElementById("boton");
+		
+
+
+function miFuncion(){
+
+	if(video.paused){
+		video.play();
+					
+		document.getElementById('play').src = "IMG_FE/boton_pause.png";
+					
+	} else {
+		video.pause();
+		document.getElementById('play').src = 'IMG_FE/boton_play.png';
+		}
+	}
