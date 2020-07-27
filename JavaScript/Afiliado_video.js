50a8c4fd5acb7df2376d8fce5c265849923215bd
Sagui10 <marc10sagues@gmail.com>
2020-07-27 19:24:17 +0200

    aaa

--- JavaScript/Afiliado_video.js
+++ JavaScript/Afiliado_video.js
@@ -1,9 +1,28 @@
 
-var video = document.getElementById("video1");
-var boton = document.getElementById("boton");
 document.getElementById('play').style.visibility = "hidden";
+
+$('#boton').click(function(){
+
+miFuncion();
+});
+$('#boton2').click(function(){
+var video = document.getElementById("video2");
+var boton = document.getElementById("boton2");
+miFuncion2();
+});
+$('#boton3').click(function(){
+var video = document.getElementById("video3");
+var boton = document.getElementById("boton3");
+miFuncion3();
+});
+
+
+
+
 	
 function aparecePlay() {
+	var video = document.getElementById("video1");
+var boton = document.getElementById("boton");
 
 	if(video.paused)  {
 
@@ -22,8 +41,52 @@ function aparecePlay() {
 
 
  }
+ function aparecePlay2() {
+ 	var video = document.getElementById("video2");
+var boton = document.getElementById("boton2");
+
+	if(video.paused)  {
+
+		document.getElementById('play2').style.visibility = "visible";			
+		document.getElementById('play2').src = "IMG_FE/boton_play_3.jpg";
+				document.getElementById('play2').style.width = "7vw";
+
+	} else {
+
+		document.getElementById('play2').style.visibility = "visible";			
+		document.getElementById('play2').src = "IMG_FE/pause-2.png";
+				document.getElementById('play2').style.width = "4vw";
+
+
+	}
+
+
+ }
+ function aparecePlay3() {
+ 	var video = document.getElementById("video3");
+var boton = document.getElementById("boton3");
+
+	if(video.paused)  {
+
+		document.getElementById('play3').style.visibility = "visible";			
+		document.getElementById('play3').src = "IMG_FE/boton_play_3.jpg";
+				document.getElementById('play3').style.width = "7vw";
+
+	} else {
+
+		document.getElementById('play3').style.visibility = "visible";			
+		document.getElementById('play3').src = "IMG_FE/pause-2.png";
+				document.getElementById('play3').style.width = "4vw";
+
+
+	}
+
+
+ }
 
 function miFuncion(){
+	var video = document.getElementById("video1");
+var boton = document.getElementById("boton");
 
 	if(video.paused){
 		
@@ -31,8 +94,8 @@ function miFuncion(){
 		video.play();
 		document.getElementById('play').style.visibility = "visible";
 
-		document.getElementById('play').src = "IMG_FE/boton_play_3.jpg";
-				document.getElementById('play').style.width = "7vw";
+		document.getElementById('play').src = "IMG_FE/pause-2.png";
+		document.getElementById('play').style.width = "4vw";
 
 
 			
@@ -42,6 +105,52 @@ function miFuncion(){
 		document.getElementById('play').style.visibility = "visible";
 		document.getElementById('play').src = 'IMG_FE/boton_play_3.jpg';
 		document.getElementById('play').style.width = "7vw";
+	}
+}
+function miFuncion2(){
+	var video = document.getElementById("video2");
+var boton = document.getElementById("boton2");
+
+	if(video.paused){
+		
+			
+		video.play();
+		document.getElementById('play2').style.visibility = "visible";
+
+		document.getElementById('play2').src = "IMG_FE/pause-2.png";
+		document.getElementById('play2').style.width = "4vw";
+
+
+			
+					
+	} else {
+		video.pause();
+		document.getElementById('play2').style.visibility = "visible";
+		document.getElementById('play2').src = 'IMG_FE/boton_play_3.jpg';
+		document.getElementById('play2').style.width = "7vw";
+	}
+}
+function miFuncion3(){
+	var video = document.getElementById("video3");
+var boton = document.getElementById("boton3");
+
+	if(video.paused){
+		
+			
+		video.play();
+		document.getElementById('play3').style.visibility = "visible";
+
+		document.getElementById('play3').src = "IMG_FE/pause-2.png";
+		document.getElementById('play3').style.width = "4vw";
+
+
+			
+					
+	} else {
+		video.pause();
+		document.getElementById('play3').style.visibility = "visible";
+		document.getElementById('play3').src = 'IMG_FE/boton_play_3.jpg';
+		document.getElementById('play3').style.width = "7vw";
 	}
 }
 
@@ -59,6 +168,14 @@ if (video.paused){
      	document.getElementById('play').style.visibility = "hidden";			
 		document.getElementById('play').src = "IMG_FE/pause-2.png";
 				document.getElementById('play').style.width = "4vw";
+
+		document.getElementById('play2').style.visibility = "hidden";			
+		document.getElementById('play2').src = "IMG_FE/pause-2.png";
+				document.getElementById('play2').style.width = "4vw";
+
+		document.getElementById('play3').style.visibility = "hidden";			
+		document.getElementById('play3').src = "IMG_FE/pause-2.png";
+				document.getElementById('play3').style.width = "4vw";
 
 
 
